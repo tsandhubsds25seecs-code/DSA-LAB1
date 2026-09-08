b81f3214a067fc9fc75e30af5a62ef12bf47998e
tsandhubsds25seecs-code <tsandhu.bsds25seecs@seecs.edu.pk>
2026-09-08 22:06:37 +0500

    Add files via upload

---
+++ DSA-LAB1.cpp
@@ -0,0 +1,10 @@
+// DSA-LAB1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to DSA Lab 1!" << endl;
+    return 0;
+}
